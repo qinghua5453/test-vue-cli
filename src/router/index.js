171ea19c505bdb7d366478d557c2a9eb0fb30c9a
jian.zhang <545300506@qq.com>
2021-01-29 11:20:39 +0800

    modify router-history

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-27 15:55:02
- * @LastEditTime: 2021-01-27 18:27:38
+ * @LastEditTime: 2021-01-29 11:19:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /qie-node-open/Users/zhangjian/practice/test-vue-cli/src/router/index.js
@@ -9,7 +9,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
-const router = new VueRouter({routes});
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
 Vue.use(VueRouter);
 export default router;
 
